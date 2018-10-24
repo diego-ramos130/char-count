4724407199ef4770099e3edefd14e43479d16bfe
diego-ramos130 <diego.onethreezero@gmail.com>
2018-10-24 14:36:02 -0700

    initial commit with initial function

---
+++ src/lib/charCount.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = string => {
+  const countMap = new Map();
+  for (let i = 0; i < string.length - 1; i++) {
+    if (countMap.get(string[i])) {
+      countMap.set(string[i], countMap.get(string[i]) + 1);
+    } else {
+      countMap.set(string[i], 1);
+    }
+  }
+  return countMap;
+};
